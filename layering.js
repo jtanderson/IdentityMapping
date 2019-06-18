90f987387fb68e48e9ad7d2a54862c31a1fa2c60
Sam Barnes <sbarnes7@gulls.salisbury.edu>
2019-06-18 10:15:57 -0400

    Revert layering JS
    Updating to my last change six days agao, need to go back and add in Grace's changes manually.

--- layering.js
+++ layering.js
@@ -1,194 +1,117 @@
-/*
- *  Set up the circle objects
- *  TODO: make starting centers random
- */
-/* TODO: loop this block instead of copy-paste
- * see text section for example
- */
-// has all the circles
-var circleLayer = new Layer();
+var circleLayer = new Layer();//creates the circle layer
 var min = 55;
 var max = 135;
 var minR = 125;
 var maxR = 650;
-for(var i=1; i<=5; i++){
-  //PROBLEM: when using random get crazy amount of intersections
-  // jta: this is okay for now, the won't all be displayed initially anyway
+for(var i=1; i<=5; i++){//loop for circle creation (random)
   var circle = new Path.Circle({
     center: [Math.floor(Math.random() * (+maxR - +minR)) + +minR, Math.floor(Math.random() * (+maxR - +minR)) + +minR],
     radius: Math.floor(Math.random() * (+max - +min)) + +min,
-    fillColor: 'white',
     fillColor: new Color(1, 1, 1, 0.75), //'white',
     strokeColor: 'black',
     id: i,
     insert: false,
+    visible: false,
     data: {
       circleId: i
     }
   });
-  circleLayer.addChild(circle); 
+  circleLayer.addChild(circle); //add each circle to the layer
 }
 circleLayer.data.layerName = "circles";
-project.addLayer(circleLayer);
+project.addLayer(circleLayer);//adds the layer
 
-// In console, can use the "getItem" and leverage the data field
-// e.g., paper.project.getItem({data:{layerName: "circles"}}).getItem({data:{circleId: "1"}})
-
-var intersectionLayer = new Layer();
+var intersectionLayer = new Layer();//starts the intersection layer
 intersectionLayer.data.layerName = "intersections";
-for(var i=1;i<6;i++){
-  
-  var c_i = project
-    .getItem({data: {layerName: "circles"}})
-    .getItem({data: {circleId: i}});
-    
-  for(var j=i+1;j<6;j++){
-    
-    console.log("checking " + i + " and " + j);
-    
-    var c_j = project
-    .getItem({data: {layerName: "circles"}})
-    .getItem({data: {circleId: j}});
-
-    var int_ij = c_i.intersect(c_j, {insert: false});
-    int_ij.data.id = ""+i+j;
-    intersectionLayer.addChild(int_ij);
-    console.log(int_ij);
-
-    for(var k = j+1; k<6; ++k){
-      
-      console.log("Checking intersection of " + i + " " + j + " " + k);
-      
-      var c_k = project
-      .getItem({data: {layerName: "circles"}})
-      .getItem({data: {circleId: k}});
-
-      var int_ijk = c_i.intersect(c_j_k, {insert: false});
-      int_ijk.data.id = ""+i+j+k;
-      intersectionLayer.addChild(int_ijk);
-      console.log(int_ijk);
-     
-      for(var l = k+1; l<6; l++){
-	
-        console.log("Checking intersection of " + i + " " + j + " " + k + " " + l);
-	
-	var c_l = project
-	.getItem({data: {layerName: "circles"}})
-	.getItem({data: {circleId: l}});
-	
-	var int_ijkl = c_i.intersect(c_j_k_l, {insert: false});
-	int_ijkl.data.id = ""+i+j+k+l;
-	intersectionLayer.addChild(int_ijkl);
-	console.log(int_ijkl);
-	
-	for(var m = l+1; m < 6; m++){
-	  
-	 console.log("Checking intersection of " + i + " " + j + " " + k + " " + l + " " + m); 
-	 
-	 var c_m = project
-	 .getItem({data: {layerName: "circles"}})
-	 .getItem({data: {circleId: m}});
-	 
-	 var int_ijklm = c_i.intersect(c_j_k_l_m, {insert: false});
-	 int_ijklm.data.id = ""+i+j+k+l+m;
-	 intersectionLayer.addChild(int_ijklm);
-	 console.log(int_ijklm);
-	 
-	}//end of four intersection
-      }//end of third intersection
-    }//end of two intersection
-  }//end of single intersection
-  
-  //TODO: Quintuple
-  
-  
-  
-  
-  /*
-  for(var k = 1; k < 3; k++){
-    var c_k = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: k}});
-    var int_ijk = int_ij.intersect(c_k, {insert: false});
-    int_ijk.data.id = ""+i+j+k;
-    intersectionLayer.addChild(int_ijk);
-    console.log(int_ijk);
-  }
-  for(var l = 1; k < 2; k++){
-    var c_l = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: l}});
-    var int_ijkl = int_ijk.intersect(c_l, {insert: false});
-    int_ijkl.data.id = ""+i+j+k+l;
-    intersectionLayer.addChild(int_ijkl);
-    console.log(int_ijkl);
-  }
-  var c_m = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: m}});
-  var int_ijklm = int_ijkl.intersect(c_m, {insert: false});
-  int_ijklm.data.id = ""+i+j+k+l+m;
-  intersectionLayer.addChild(int_ijklm);
-  console.log(int_ijklm);
-  */
-}
-
-//TODO: do intersection of all five
-
-//layer for adding text and manipulating text, will be the top layer, grouping should be here as it ties everything together
-
-var textLayer = new Layer();
+intersections();
+var textLayer = new Layer();//creates the text layer which will be the top layer
 textLayer.data.layerName = "text";
-//console.log("**************************");
-//console.log(project.layers);
-//project.layers[2].data.layerName = "textLayer";
-for(var i=1; i<=5; i++){
+
+
+
+for(var i=1; i<=5; i++){//loops the five text creation and binds to the circle objects position
   var c = project.getItem({data: {circleId: i}});
   var text = new PointText({
-    //point: [100,100],
     fillColor:  'black',
     content:  "Circle " + i,
     position: c.position,
     insert: false,
+    visible: false,
     data: {
       textId: i
     }
   });
-  textLayer.addChild(text);
+
+  textLayer.addChild(text);//adds text to the text layer
 }
+
+//function for creating intersections
+function intersections(){
+  for(var i=1;i<6;i++){
+    var c_i = project
+      .getItem({data: {layerName: "circles"}})
+      .getItem({data: {circleId: i}});
+    for(var j=i+1;j<6;j++){
+      var c_j = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: j}});
+
+      var int_ij = c_i.intersect(c_j, {insert: false});
+      int_ij.data.id = ""+i+j;
+      intersectionLayer.addChild(int_ij);
+      for(var k = j+1; k<6; ++k){
+           var c_k = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: k}});
+           var int_ijk = int_ij.intersect(c_k, {insert: false});
+          int_ijk.data.id = ""+i+j+k;
+          intersectionLayer.addChild(int_ijk); 
+        for(var l=k+1; l<6; l++){
+          var c_l = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: l}});
+      var int_ijkl = int_ijk.intersect(c_l, {insert: false});
+      int_ijkl.data.id = ""+i+j+k+l;
+      intersectionLayer.addChild(int_ijkl);
+        }
+      }
+    }
+  }//end intersections loop
+
+//
+   var c_m = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: 5}});//5-tuple intersection
+    var int_ijklm = int_ijkl.intersect(c_m, {insert: false});
+    int_ijklm.data.id = ""+i+j+k+l+5;
+    intersectionLayer.addChild(int_ijklm);
+}//end intersections function
+
+//function for fixing the layers
 var fixLayers = function(){
-  // because of binding, may need to use project.layers...
   textLayer.sendToBack();
   intersectionLayer.sendToBack();
   circleLayer.sendToBack();
   console.log('Fixed layers...');
-  //console.log(textLayer);
-  //console.log(intersectionLayer);
-  //console.log(circleLayer);
 }
+
 var activeItem; 
 var handle;
 var dragged = false; 
-//end layering manipulation, starts functions and such
-/*
- * 1. Need to hit test on items within the layer not the entire layer
- * 2. Active item setting is probably wrong since setting entire layer and not one item 
- */
+var tester = false;
+//function when user makes a click
 function onMouseDown(event){
+  if(activeItem){
+    activeItem.selected = false;
+   //need something else here maybe
+  }
   handle = null;
   var cLayer = project.getItem({data: {layerName: "circles"}});
   var iLayer = project.getItem({data: {layerName: "intersections"}});
 
-  if(hitResult == iLayer.hitTest(event.point)){//if the intersection layer is hit
-    activeItem = iLayer;//needs to be a certain int not entire layer but not sure how to test which int
-    console.log("Active item has children:");
-    console.log(activeItem.children);
-  }
-  else if(hitResult == cLayer.hitTest(event.point)){//if the circle layer is hit
-    activeItem = cLayer;//same as above need to set one item to active not entire layer
   // first assigns the test to hitResult
   // second, evaluates whether it was null or not
   if(hitResult = iLayer.hitTest(event.point)){//if the intersection layer is hit
     activeItem = hitResult.item; // will be a intersection
+    activeItem.selected = true;
   } else if(hitResult = cLayer.hitTest(event.point)){//if the circle layer is hit
     //activeItem = cLayer;//same as above need to set one item to active not entire layer
     activeItem = hitResult.item; // will be a circle
+    tester = true;
     activeItem.selected = true;
-
+ 
     //creates circles handle
     handle = activeItem.hitTest(event.point, 
       {
@@ -198,57 +121,30 @@ function onMouseDown(event){
         tolerance: 5
       }
     );
-    console.log("Active item has children:");
-    console.log(activeItem.children);
-  }
-  else{
   } else {
     console.log("Nothing hit");
-    for(var i in iLayer){
-      i.selected = false;//??
+    activeItem = null;
     for(var i in iLayer.children){
-      iLayer.children[i].selected = false; //??
+      iLayer.children[i].selected = false; 
     }
-    for(var c in cLayer){
-      c.selected = false;//??
     for(var i in cLayer.children){
-      cLayer.children[i].selected = false; //??
+      cLayer.children[i].selected = false; 
     }
   }
   fixLayers();
-}
+}//end of the mouse down function
+
 var segment;
-
-
-/*
-
-//gmb
-//function for deleting intersections, need to be more complicated???
-var a;
-function removeIntersection(a){
-  
-  delete a;
-  
-}
-
-
-*/
-
-function onMouseDrag(event) {
-  dragged = true;
-
-  // @Grace, as soon as a drag starts, destroy all existing intersections
-  // they will be re-created on the mouseUp event
-
- // removeIntersection(int_ij);
-  //repeat with further intersections
-  
-  if ( activeItem){
-    // terrible and dirty.
-    if( activeItem.isIntersection == "true" ){
-      return false;
+//when user clicks and drags
+function onMouseDrag(event){//needs a boolean value for what is clicked and dragged
+  dragged = true;//not sure if we still need this yet
+  //TODO: need to destroy old intersections here before we test and make any changes
+  var cLayer = project.getItem({data: {layerName: "circles"}});
+  var iLayer = project.getItem({data: {layerName: "intersections"}});
+  if(tester){//if it is a circle being hit
+    for(var i in iLayer.children){
+      iLayer.children[i].remove(); 
     }
-    //if we hit a handle, resize the circle
     if(handle && (handle.type == 'stroke' || handle.type == 'segment')){
       var p = event.point; // old
       var p2 = p + event.delta; // new
@@ -261,111 +157,98 @@ function onMouseDrag(event) {
     }
     //else move the circle
     else {
-      //activeItem.translate(event.point - activeItem.position);
-      // todo: only do this if movement isn't locked
-      console.log(activeItem);
-      console.log("==========");
-      //console.log(activeItem);
-      //console.log("==========");
-      activeItem.translate(event.delta);// + activeItem.position);
+      var data = activeItem.data.circleId;
+      activeItem.translate(event.delta);
+      project.getItem({data: {textId: data}}).translate(event.delta);
+      // + activeItem.position);
     }
-
-  } // end if(activeitem)
-} // end onMouseDrag
-function onMouseUp(event){
-  console.log(activeItem);
-  /*
-   * activeItem is about to get removed!
-   * need to remember which intersection it is, if any, and re-select it later
-   * add an attribute, or use "isSelected" ?
-   */
-  //dragged = true;
-  if (dragged) { 
-    console.log("Calculating intersections...");
-    
-    //TODO: Compare w regular remove, why was this code not working before?
-    for (var _int in intersections){
-      intersections[_int].remove();
-    }
-    
-    
-    
-    
-    // @Grace, compare w/ code above, put into own function
-    // iterate every 2, 3, 4-tuple and calculate intersection of all
-    // need to add the 5-tuple after
-    /*
-    for(var i=1;i<6;i++){
-      for(var j=i+1;j<6;j++){
-        if( groupArray[j].visible ){
-          intersections[""+i+j] = groupArray[i].children[0].intersect(groupArray[j].children[0]);
-          // TODO: more of this
-          intersections[""+i+j].data["intersectionId"] = "blah";
-          for(var k=j+1;k<6;k++){
-            intersections[""+i+j+k] = intersections[""+i+j].intersect(groupArray[k].children[0]);
-            for(var l =k+1; l < 6; l++){
-              intersections[""+i+j+k +l] = intersections[""+i+j + k].intersect(groupArray[l].children[0]);
-              
-              
-              */
-    
-    
-    
-    
-    
-    
-            }
-          }
-        }
-      }
-    }
+  } else{
+    return false;
   }
-  dragged = false; // reset
-  console.log("Calling fixLayers...");
+}//end mouse dragging function
+
+//on mouse up function
+function onMouseUp(event){
+  var iLayer = project.getItem({data: {layerName: "intersections"}});
+  var cLayer = project.getItem({data: {layerName: "circles"}});
+  var visibility = true;
+  for(var j in cLayer.children){
+    if(cLayer.children[j].visible == false){
+      visibility = false;
+    }
+  }  
+  if(visibility == true){
+      intersections();
+
+  }
+
+  //need to de select active item
+  //TODO: recalculate all intersections
+  if(dragged){//if the user dragged the circle
+    //remove all old intersections
+    for(var i in iLayer.children){
+      iLayer.children[i].remove(); 
+    }
+    //create new intersections here
+    //calculate new intersections by calling graces function
+  }
+  dragged = false;
   fixLayers();
-}
+}//end mouse up function
+
 var scope = this;
+
 /*
  * Called when "Add Circle" is clicked
  */
 doSubmit = function(e){
   scope.activate();
   e.preventDefault();
-  console.log(e);
+
+
   // the id of the circle we're changing
   var targetName = e.target.querySelector("[name=formId]").value.toLowerCase();
   // holds the user's text entry
   var text = e.target.getElementsByTagName("input")[0].value;
+  localStorage[targetName] = text;
   console.log("Looking for circle " + targetName);
+
   var obj = project
   //.getItem({data: {layerName: "circles"}})
     .getItem({data: {circleId: parseInt(targetName)}});
+    var objText = project
+    //.getItem({data: {layerName: "circles"}})
+      .getItem({data: {textId: parseInt(targetName)}});
+  obj.visible = true;
+  objText.visible = true;
   console.log(obj);
   var t = project.getItem({
     //_class: "PointText"
     data: {textId: parseInt(targetName)}
   });
   t.content = text;
+
   // TODO: instead of changing visibility, call "insert" if not already there
   obj.visible = true;
   return false;
 } //end doSubmit function
+
 window.doSubmit= doSubmit;
+//handles all color sliders as well as outlines
 var sliderIntersect=document.getElementById("rangeIntersect");
 sliderIntersect.addEventListener("change",function(){
   var r,b;
   r=Math.round(255*(100-sliderIntersect.value)/100);
   b=Math.round(255*sliderIntersect.value/100);
-  // doesn't work for intersections because then activeItem doesn't have children
-  if( !activeItem.isIntersection ){
-    activeItem.children[0].fillColor = "rgb("+r+",0,"+b+")";
+  var cLayer = project.getItem({data: {layerName: "circles"}});
+  if(hitResult = cLayer.hitTest(event.point)){
+    activeItem = hitResult.item; // will be a circle
+        activeItem.children[0].fillColor = "rgb("+r+",0,"+b+")";
     if(activeItem.value == 0){ // TODO: you mean slider value here?
       activeItem.children[0].fillColor = "white";
     }
-  } else {
-    // activeItem is an intersection (i.e. some Path object)
-    activeItem.fillColor = "rgb("+r+",0,"+b+")";
   }
+    activeItem.fillColor = "rgb("+r+",0,"+b+")";
 },false);
 var formIntersect1 = document.getElementById("inlineRadioIntersect1");
 formIntersect1.addEventListener("change",function(){
