1aab85fc043c95bddf8dfa743dbaf386d7f1a8ec
Joe Anderson <jtanderson@salisbury.edu>
2019-06-19 09:05:50 -0400

    Fix some laying issues... uncover other issues

--- layering.js
+++ layering.js
@@ -7,7 +7,7 @@ for(var i=1; i<=5; i++){//loop for circle creation (random)
   var circle = new Path.Circle({
     center: [Math.floor(Math.random() * (+maxR - +minR)) + +minR, Math.floor(Math.random() * (+maxR - +minR)) + +minR],
     radius: Math.floor(Math.random() * (+max - +min)) + +min,
-    fillColor: new Color(1, 1, 1), //took out opacity to test intersections
+    fillColor: new Color(1, 1, 1, 0.75), //took out opacity to test intersections
     strokeColor: 'black',
     id: i,
     insert: false,
@@ -51,35 +51,64 @@ function intersections(){
     var c_i = project
       .getItem({data: {layerName: "circles"}})
       .getItem({data: {circleId: i}});
-    for(var j=i+1;j<6;j++){
-      var c_j = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: j}});
-      var int_ij = c_i.intersect(c_j, {insert: false});
-      int_ij.data.id = ""+i+j;
-      intersectionLayer.addChild(int_ij); //2
-      for(var k=j+1;k<6;k++){
-        var c_k = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: k}});
-        var int_ijk = int_ij.intersect(c_k, {insert: false});
-        int_ijk.data.id = ""+i+j+k;
-        intersectionLayer.addChild(int_ijk); //3
-        for(var l = k+1;l<6; l++){
-          var c_l = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: l}});
-          var int_ijkl = int_ijk.intersect(c_l, {insert: false});
-          int_ijkl.data.id = ""+i+j+k+l;
-          intersectionLayer.addChild(int_ijkl);//4
-        }
-      }
+    if( c_i.visible ){
+      for(var j=i+1;j<6;j++){
+        var c_j = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: j}});
+        if( c_j.visible ){
+          var int_ij = c_i.intersect(c_j, {insert: false});
+          int_ij.data.id = ""+i+j;
+          //int_ij.fillColor = new Color(1,0,0);
+          intersectionLayer.addChild(int_ij); //2
+          for(var k=j+1;k<6;k++){
+            var c_k = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: k}});
+            if( c_k.visible ){
+              var int_ijk = int_ij.intersect(c_k, {insert: false});
+              //int_ijk.fillColor = new Color(0,1,0);
+              int_ijk.data.id = ""+i+j+k;
+              intersectionLayer.addChild(int_ijk); //3
+              for(var l = k+1;l<6; l++){
+                var c_l = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: l}});
+                if( c_l.visible ){
+                  var int_ijkl = int_ijk.intersect(c_l, {insert: false});
+                  //int_ijkl.fillColor = new Color(0,0,1);
+                  int_ijkl.data.id = ""+i+j+k+l;
+                  intersectionLayer.addChild(int_ijkl);//4
+                } // c_l visible
+              } // l loop
+            } // c_k visible
+          } // k loop
+        } // c_j visible
+      } // j loop
+    } // c_i visible
+  } // i loop
+
+  var c_m = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: 5}});//5-tuple intersection
+  if( c_m.visible ){
+    var int_ijklm = intersectionLayer.getItem({data: {id: "1234"}}).intersect(c_m, {insert: false});
+    if( int_ijklm ){
+      int_ijklm.data.id = ""+i+j+k+l+"5";
+      //int_ijklm.fillColor = new Color(1,1,0);
+      intersectionLayer.addChild(int_ijklm);
     }
   }
-  var c_m = project.getItem({data: {layerName: "circles"}}).getItem({data: {circleId: 5}});//5-tuple intersection
-  var int_ijklm = int_ijkl.intersect(c_m, {insert: false});
-  int_ijklm.data.id = ""+i+j+k+l+5;
-  intersectionLayer.addChild(int_ijklm);
 }//end intersections function
 
 //function for fixing the layers
 var fixLayers = function(){
+  // do the intersections
+  // TODO: fix, doesn't seem to work sometimes
+  var iLayer = project.getItem({data: {layerName: "intersections"}});
+  for(var i = 2; i<6; i++){
+    for(var j=0; j<iLayer.children.length; j++){
+      if( iLayer.children[j].data.id.length == i ){
+        iLayer.children[j].bringToFront();
+      }
+    }
+  }
+  
+  // TODO: get fresh
   textLayer.sendToBack();
-  intersectionLayer.sendToBack();
+  iLayer.sendToBack();
   circleLayer.sendToBack();
   console.log('Fixed layers...');
 }//end fix layers function
@@ -100,6 +129,7 @@ function onMouseDown(event){
 
   // first assigns the test to hitResult
   // second, evaluates whether it was null or not
+  // TODO: do not test visible items!!!!
   if(hitResult = iLayer.hitTest(event.point)){//if the intersection layer is hit
     activeItem = hitResult.item; // will be a intersection
     activeItem.selected = true;
@@ -139,9 +169,7 @@ function onMouseDrag(event){//needs a boolean value for what is clicked and drag
   var cLayer = project.getItem({data: {layerName: "circles"}});
   var iLayer = project.getItem({data: {layerName: "intersections"}});
   if(tester){//if it is a circle being hit
-    for(var i in iLayer.children){
-      iLayer.children[i].remove(); 
-    }
+    iLayer.removeChildren();
     if(handle && (handle.type == 'stroke' || handle.type == 'segment')){
       var p = event.point; // old
       var p2 = p + event.delta; // new
@@ -159,9 +187,10 @@ function onMouseDrag(event){//needs a boolean value for what is clicked and drag
       project.getItem({data: {textId: data}}).translate(event.delta);
       // + activeItem.position);
     }
-  } else{
-    return false;
   }
+
+  //intersections();
+  fixLayers();
 }//end mouse dragging function
 
 //on mouse up function
@@ -219,6 +248,7 @@ doSubmit = function(e){
 
   // TODO: instead of changing visibility, call "insert" if not already there
   obj.visible = true;
+  intersections();
   return false;
 } //end doSubmit function
 
