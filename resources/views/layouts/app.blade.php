4fde71abf626d7df4998e4044b6f4dec85a9e2d1
Grace Burton <gburton5@gulls.salisbury.edu>
2020-03-03 11:52:23 -0500

    Got rid of inline styling/JS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,61 +1,61 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta http-eqiuv="Content-Security-Policy" content="default-src *  data: blob: 'unsafe-inline' 'unsafe-eval';">
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+<head>
+  <meta http-eqiuv="Content-Security-Policy" content="default-src *  data: blob: 'unsafe-inline' 'unsafe-eval';">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
 
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Identity Mapping') }}</title>
+  <title>{{ config('app.name', 'Identity Mapping') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">  
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <!-- Styles -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">  
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-        <style>
-        @yield('css')
-        </style>
-    </head>
-    <body>
+  <style>
+    @yield('css')
+  </style>
+</head>
+<body>
 
-      <div class="progress" style = "margin-bottom: 30px;">
-        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: {{ $progress }}%;">
-          <span class="sr-only">{{ $progress }}% Complete</span>
+  <div class="progress" style = "margin-bottom: 30px;">
+    <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: {{ $progress }}%;">
+      <span class="sr-only">{{ $progress }}% Complete</span>
+    </div>
+  </div>
+
+  <div id="app" class="container">
+
+    @yield('content')
+
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="text text-left">
+          @if ($prevURL != '')
+          <h4><a href="{{ $prevURL }}">&larr;&nbsp; Previous</a></h4>
+          @endif
         </div>
       </div>
-
-        <div id="app" class="container">
-            <div class="row">
-              <div class="col-sm-2" style="">
-                  <a class="btn btn-danger" href="finished.html">Abort</a>
-              </div>
-            </div>
-
-            @yield('content')
-
-            <div class="row">
-              <div class="col-sm-6">
-                <div class="text text-left">
-                  @if ($prevURL != '')
-                  <h4><a href="{{ $prevURL }}">&larr;&nbsp; Previous</a></h4>
-                  @endif
-                </div>
-              </div>
-              <div class="col-sm-6">
-                <div class="text text-right">
-                  @if ($nextURL != '')
-                  <h4><a href="{{ $nextURL }}">Next &rarr;</a>&emsp;&emsp;</h4>
-                  @endif
-                </div> 
-              </div>
-            </div>
+      <div class="col-sm-4">
+        <div class="text text-left">
+          <a class="btn btn-danger" href="">Abort</a>
         </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="text text-right">
+          @if ($nextURL != '')
+          <h4><a href="{{ $nextURL }}">Next &rarr;</a>&emsp;&emsp;</h4>
+          @endif
+        </div> 
+      </div>
+    </div>
+  </div>
 
-        <!-- Scripts -->
-        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
-        @yield('javascript')
-    </body>
+  <!-- Scripts -->
+  <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+  @yield('javascript')
+</body>
 </html>
--- resources/views/position.blade.php
+++ resources/views/position.blade.php
@@ -1,121 +1,119 @@
 @extends('layouts.app')
 
 @section('css')
-#c {
-  border-style: solid;
-  width: 750px;
-  height: 750px;
-}
+  #c {
+    border-style: solid;
+    width: 750px;
+    height: 750px;
+  }
 @endsection
 
 @section('content')
-      <div class="row">
-        <div class="col-sm">
-          <h1><p text-center>Map your Identities</p>   </h1>   
-      <div class="text text-right">
- 
+<div class="row">
+  <div class="col-sm">
+    <h1><p text-center>Map your Identities</p>   </h1>   
+    <div class="text text-right">
+     
+    </div>
+    <div id="StartMapping" class="tabcontent">     
+      This is the section where you choose the five social identities that are closest to you and map them onto the canvas where you see fit.
+      To add social identities, simply type each one into the corresponding text box then click add circle. Once a circle has been added you can move them on the screen
+      to be placed where you think they fit best.
+      <br><br>
+    </div> 
+    <div class="row">
+      <div class="col-sm-4">
+        <br><br>
+        <form action="#" onsubmit="doSubmit(event);">
+          <input type="text" name="circleText" id="textInput" style="width: 60%;">
+          <input type="hidden" name="formId" value="1"><!-- holds the SUid corresponding to this circle -->
+          <input type="submit" value="Add circle">
+        </form>
+        <br><br>
+        <form action="#" onsubmit="doSubmit(event);">
+          <input type="text" name="circleText" id="textInput" style="width: 60%;">
+          <input type="hidden" name="formId" value="2"><!-- holds the SUid corresponding to this circle-->
+          <input type="submit" value="Add circle">
+        </form>
+        <br><br>
+        <form action="#" onsubmit="doSubmit(event);">
+          <input type="text" id="textInput" style="width: 60%;">
+          <input type="hidden" name="formId" value="3"><!-- holds the SUid corresponding to this circle-->      
+          <input type="submit" value="Add circle">
+        </form>
+        <br><br>
+        <form action="#" onsubmit="doSubmit(event);">
+          <input type="text" id="textInput" style="width: 60%;">
+          <input type="hidden" name="formId" value="4"><!-- holds the SUid corresponding to this circle-->      
+          <input type="submit" value="Add circle">
+        </form>
+        <br><br>
+        <form action="#" onsubmit="doSubmit(event);">
+          <input type="text" id="textInput" style="width: 60%;">
+          <input type="hidden" name="formId" value="5"><!-- holds the SUid corresponding to this circle-->      
+          <input type="submit" value="Add circle">
+        </form>
+        
+        <br>
+        <br>
+        <br><br>
       </div>
-          <div id="StartMapping" class="tabcontent">     
-            This is the section where you choose the five social identities that are closest to you and map them onto the canvas where you see fit.
-            To add social identities, simply type each one into the corresponding text box then click add circle. Once a circle has been added you can move them on the screen
-            to be placed where you think they fit best.
-            <br>
-          </div> 
-          <div class="container">
-            <div class="row">
-              <div class="col-sm-4">
-                <br><br>
-                <form action="#" onsubmit="doSubmit(event);">
-                  <input type="text" name="circleText" id="textInput" style="width: 60%;">
-                  <input type="hidden" name="formId" value="1"><!-- holds the SUid corresponding to this circle -->
-                  <input type="submit" value="Add circle">
-                </form>
-<br><br>
-                <form action="#" onsubmit="doSubmit(event);">
-                  <input type="text" name="circleText" id="textInput" style="width: 60%;">
-                  <input type="hidden" name="formId" value="2"><!-- holds the SUid corresponding to this circle-->
-                  <input type="submit" value="Add circle">
-                </form>
-<br><br>
-                <form action="#" onsubmit="doSubmit(event);">
-                  <input type="text" id="textInput" style="width: 60%;">
-                  <input type="hidden" name="formId" value="3"><!-- holds the SUid corresponding to this circle-->      
-                  <input type="submit" value="Add circle">
-                </form>
-<br><br>
-                <form action="#" onsubmit="doSubmit(event);">
-                  <input type="text" id="textInput" style="width: 60%;">
-                  <input type="hidden" name="formId" value="4"><!-- holds the SUid corresponding to this circle-->      
-                  <input type="submit" value="Add circle">
-                </form>
-                <br><br>
-                <form action="#" onsubmit="doSubmit(event);">
-                  <input type="text" id="textInput" style="width: 60%;">
-                  <input type="hidden" name="formId" value="5"><!-- holds the SUid corresponding to this circle-->      
-                  <input type="submit" value="Add circle">
-                </form>
-          
-                <br>
-                <br>
-                <br><br>
-              </div>
-<script>
-  function doFunction(){
-    locked = true;
-    console.log(locked);
-  }
-</script>
-              <div class="col-sm-8">
-                <canvas id="c" resize></canvas>
-                <div class="text text-right">
-                  </div>
-              </div>
-            </div>
-          </div>
-          <br>   
-        </div></div>
-
-@endsection
-
-
-@section('javascript')
-<script type="text/javascript" src="{{ asset('js/paper.js') }}"></script>
-<script type="text/paperscript" src="{{ asset('js/layering.js') }}" canvas="c"></script>
-<script type="text/javascript">
-      var save = function(){
-        var tLayer = paper.project.getItem({data: {layerName: "text"}});
-        localStorage["saved"] = paper.project.exportJSON();
-        localStorage["one"] = tLayer.getItem({data: {textId: 1}}).content;
-        localStorage["two"] = tLayer.getItem({data: {textId: 2}}).content;
-        localStorage["three"] = tLayer.getItem({data: {textId: 3}}).content;
-        localStorage["four"] = tLayer.getItem({data: {textId: 4}}).content;
-        localStorage["five"] = tLayer.getItem({data: {textId: 5}}).content;
-        localStorage["extended"] = true;
-        var num = 1;
-        for( i in paper.project.layers[1].children){
-          if(paper.project.layers[1].children[i].closed){
-            num++;
-           }
-          }
-          var arr = new Array(num);
-          var k = 0;
-          for(j in paper.project.layers[1].children){
-            if(paper.project.layers[1].children[j].closed){
-                arr[k] = paper.project.layers[1].children[j].data.id;
-                console.log(arr[k]);
-                k++;
-            }
-          }
-          localStorage["numIntersections"] = num;
-          localStorage["intersections"] = arr;
-      }
-        var reset = function(){
-          var cLayer = paper.project.getItem({data: {layerName: "circles"}});
-        var iLayer = paper.project.getItem({data: {layerName: "intersections"}});
-        var tLayer = paper.project.getItem({data: {layerName: "text"}});
-        cLayer.removeChildren();
-        iLayer.removeChildren();
-        tLayer.removeChildren();
+      <script>
+        function doFunction(){
+          locked = true;
+          console.log(locked);
         }
       </script>
-@endsection
+      <div class="col-sm-8">
+        <canvas id="c" resize></canvas>
+        <div class="text text-right">
+        </div>
+      </div>
+    </div>
+    <br>   
+  </div></div>
+
+  @endsection
+
+
+  @section('javascript')
+  <script type="text/javascript" src="{{ asset('js/paper.js') }}"></script>
+  <script type="text/paperscript" src="{{ asset('js/layering.js') }}" canvas="c"></script>
+  <script type="text/javascript">
+    var save = function(){
+      var tLayer = paper.project.getItem({data: {layerName: "text"}});
+      localStorage["saved"] = paper.project.exportJSON();
+      localStorage["one"] = tLayer.getItem({data: {textId: 1}}).content;
+      localStorage["two"] = tLayer.getItem({data: {textId: 2}}).content;
+      localStorage["three"] = tLayer.getItem({data: {textId: 3}}).content;
+      localStorage["four"] = tLayer.getItem({data: {textId: 4}}).content;
+      localStorage["five"] = tLayer.getItem({data: {textId: 5}}).content;
+      localStorage["extended"] = true;
+      var num = 1;
+      for( i in paper.project.layers[1].children){
+        if(paper.project.layers[1].children[i].closed){
+          num++;
+        }
+      }
+      var arr = new Array(num);
+      var k = 0;
+      for(j in paper.project.layers[1].children){
+        if(paper.project.layers[1].children[j].closed){
+          arr[k] = paper.project.layers[1].children[j].data.id;
+          console.log(arr[k]);
+          k++;
+        }
+      }
+      localStorage["numIntersections"] = num;
+      localStorage["intersections"] = arr;
+    }
+    var reset = function(){
+      var cLayer = paper.project.getItem({data: {layerName: "circles"}});
+      var iLayer = paper.project.getItem({data: {layerName: "intersections"}});
+      var tLayer = paper.project.getItem({data: {layerName: "text"}});
+      cLayer.removeChildren();
+      iLayer.removeChildren();
+      tLayer.removeChildren();
+    }
+  </script>
+  @endsection
