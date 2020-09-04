56fd6b8232e1ea8f076ea6bfd8fc12c88981dddb
Joe Anderson <jtanderson@salisbury.edu>
2020-09-04 16:01:06 -0400

    Fix rows on identityDebrief

--- resources/views/identityDebrief.blade.php
+++ resources/views/identityDebrief.blade.php
@@ -11,41 +11,43 @@
   <div class="col-sm">
     <p>Now that your map is complete, we are going to ask you some questions about your social identities. <b>Please respond to the following questions thinking about each identity one at a time.</b></p>
   </div>
-  @foreach ($surveyquestions as $number => $question)
-    @if ( $number % 2 == 0 )
-    <div class="row">
-      <div class="col-sm">
-        <div class="card-deck">
-    @endif
-        <div class="card text-center" style="width: 30rem;">
-          <div class="card-body">
-            <h5 class="card-title">{{ $question->text }}</h5>
-            @foreach ($circles as $index => $circle)
-              <hr/>
-              <b>{{ $circle['label'] }}</b>
-              <form> 
-                {{ $question->extreme_left }}
-                @for ($i = 1; $i <= intval($question->degrees); $i++)
-                  <div class="form-check form-check-inline">
-                    @if ( $i == $question->answer )
-                      <input type="radio" class="survey-radio"  name="circle-{{ $circle->id }}-question-{{ $question->id }}" value="{{ $i }}" checked>
-                    @else
-                      <input type="radio" class="survey-radio"  name="circle-{{ $circle->id }}-question-{{ $question->id }}" value="{{ $i }}">
-                    @endif
-                  </div>
-                @endfor
-                {{ $question->extreme_right }}
-              </form>
-            @endforeach
-          </div>
-        </div>
-    @if ( $number % 2 == 1 )
+</div>
+
+@foreach ($surveyquestions as $number => $question)
+  @if ( $number % 2 == 0 )
+  <div class="row">
+    <div class="col-sm">
+      <div class="card-deck">
+  @endif
+      <div class="card text-center" style="width: 30rem;">
+        <div class="card-body">
+          <h5 class="card-title">{{ $question->text }}</h5>
+          @foreach ($circles as $index => $circle)
+            <hr/>
+            <b>{{ $circle['label'] }}</b>
+            <form> 
+              {{ $question->extreme_left }}
+              @for ($i = 1; $i <= intval($question->degrees); $i++)
+                <div class="form-check form-check-inline">
+                  @if ( $i == $question->answer )
+                    <input type="radio" class="survey-radio"  name="circle-{{ $circle->id }}-question-{{ $question->id }}" value="{{ $i }}" checked>
+                  @else
+                    <input type="radio" class="survey-radio"  name="circle-{{ $circle->id }}-question-{{ $question->id }}" value="{{ $i }}">
+                  @endif
+                </div>
+              @endfor
+              {{ $question->extreme_right }}
+            </form>
+          @endforeach
         </div>
       </div>
+  @if ( $number % 2 == 1 )
+      </div>
     </div>
-    @endif
-  @endforeach
-</div>
+  </div>
+  @endif
+@endforeach
+
 <br>
 @endsection
 
