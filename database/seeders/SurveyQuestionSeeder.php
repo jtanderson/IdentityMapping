6c33f7f221185529d26d71e9a46788f3041a9dad
Joe Anderson <jtanderson@salisbury.edu>
2020-09-30 13:21:48 -0400

    Set active to true on surveyquestion seed

--- database/seeders/SurveyQuestionSeeder.php
+++ database/seeders/SurveyQuestionSeeder.php
@@ -24,6 +24,7 @@ class SurveyQuestionSeeder extends Seeder
           'extreme_right' => "Weakly held",
           'degrees' => '5',
           'surveyable_type' => 'circle',
+          'active' => true,
         ],
         [ 
           'text' => "Rate the distance you believe each social identity is from defining who you are.",
@@ -31,6 +32,7 @@ class SurveyQuestionSeeder extends Seeder
           'extreme_right' => "Very far",
           'degrees' => '5',
           'surveyable_type' => 'circle',
+          'active' => true,
         ],
         [ 
           'text' => "Rate how important each social identity is to the way you think about yourself",
@@ -38,6 +40,7 @@ class SurveyQuestionSeeder extends Seeder
           'extreme_right' => "Very unimportant",
           'degrees' => '5',
           'surveyable_type' => 'circle',
+          'active' => true,
         ],
         [ 
           'text' => "How often do you think about having each social identity and what you have in common with others who share that identity?",
@@ -45,6 +48,7 @@ class SurveyQuestionSeeder extends Seeder
           'extreme_right' => "Very rarely",
           'degrees' => '5',
           'surveyable_type' => 'circle',
+          'active' => true,
         ],
         [ 
           'text' => "Indicate the extent to which something that happens in your life is affected by what happens to other people who share that social identity.",
@@ -52,6 +56,7 @@ class SurveyQuestionSeeder extends Seeder
           'extreme_right' => "Very little",
           'degrees' => '5',
           'surveyable_type' => 'circle',
+          'active' => true,
         ],
         [ 
           'text' => "How proud do you feel when someone who shares your social identity accomplishes something outstanding?",
@@ -59,6 +64,7 @@ class SurveyQuestionSeeder extends Seeder
           'extreme_right' => "Not proud",
           'degrees' => '5',
           'surveyable_type' => 'circle',
+          'active' => true,
         ],
         // participant survey questions
         [
@@ -66,14 +72,16 @@ class SurveyQuestionSeeder extends Seeder
           "extreme_left" => "Very easy",
           "extreme_right" => "Very difficult",
           "degrees" => "5",
-          "surveyable_type" => "participant"
+          "surveyable_type" => "participant",
+          'active' => true,
         ],
         [
           'text' => "Did you complete the survey in a timely manner?",
           "extreme_left" => "Able to complete quickly",
           "extreme_right" => "Took a long time to complete",
           "degrees" => "5",
-          "surveyable_type" => "participant"
+          "surveyable_type" => "participant",
+          'active' => true,
         ]
       ]);
   }
